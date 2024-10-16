a95818c9958aae976f84480ae98a592d76e73e8a
pack3tsniff3r <pack3ts@protonmail.ch>
2024-10-15 20:37:16 -0400

    cors issue

--- api/server/index.js
+++ api/server/index.js
@@ -1,6 +1,7 @@
 
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+
+import cors from 'cors';
 const app = express();
 
 app.use(cors()); // Enable CORS for all origins
